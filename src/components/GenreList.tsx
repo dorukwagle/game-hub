9699f16879a6ba099fdd5538292e327714a4153a
dorukwagle <dorukwagle@gmail.com>
2023-05-14 07:51:04 +0545

    add genres list heading

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,6 +3,7 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import GenreListSkeleton from "./GenreListSkeleton";
 import getCroppedImageUrl from "../services/image-urls";
 import { useState } from "react";
+import Heading from "./Heading";
 
 interface Props {
     onSelect: (genre: Genre) => void;
@@ -15,36 +16,41 @@ const GenreList = ({ onSelect }: Props) => {
     if (error) <div></div>;
 
     return (
-        <ListGroup>
-            {isLoading && <GenreListSkeleton />}
-            {data.length > 0 &&
-                data.map((genre, index) => (
-                    <ListGroup.Item
-                        key={genre.id}
-                        active={selectedIndex === index}
-                        onClick={() => {
-                            setSelectedIndex(index);
-                            onSelect(genre);
-                        }}
-                    >
-                        <div
-                            className={
-                                "flex gap-4 items-center font-bold " +
-                                (selectedIndex === index
-                                    ? "text-lg"
-                                    : "")
-                            }
+        <>
+            <p className="text-3xl ml-4 font-bold text-gray-900 dark:text-white">
+                Genres
+            </p>
+            <ListGroup>
+                {isLoading && <GenreListSkeleton />}
+                {data.length > 0 &&
+                    data.map((genre, index) => (
+                        <ListGroup.Item
+                            key={genre.id}
+                            active={selectedIndex === index}
+                            onClick={() => {
+                                setSelectedIndex(index);
+                                onSelect(genre);
+                            }}
                         >
-                            <Avatar
-                                img={getCroppedImageUrl(genre.image_background)}
-                                rounded={true}
-                                size="sm"
-                            />
-                            {genre.name}
-                        </div>
-                    </ListGroup.Item>
-                ))}
-        </ListGroup>
+                            <div
+                                className={
+                                    "flex gap-4 items-center font-bold " +
+                                    (selectedIndex === index ? "text-lg" : "")
+                                }
+                            >
+                                <Avatar
+                                    img={getCroppedImageUrl(
+                                        genre.image_background
+                                    )}
+                                    rounded={true}
+                                    size="sm"
+                                />
+                                {genre.name}
+                            </div>
+                        </ListGroup.Item>
+                    ))}
+            </ListGroup>
+        </>
     );
 };
 export default GenreList;
