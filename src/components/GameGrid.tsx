aa897ff0e501f1e1f3bf107449195ccc2df6140f
dorukwagle <dorukwagle@gmail.com>
2023-05-14 08:34:05 +0545

    refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,10 +13,11 @@ const GameGrid = ({ gameQuery }: Props) => {
     const { data, error, isLoading } = useGames(gameQuery);
     const skeletonArrya = [1, 2, 3, 4, 5, 6];
 
+    if (error ) return <p className="text-red-600 dark:text-pink-700 font-bold text-xl">{error}</p>;
+    
     return (
         <div className="p-2 grid gap-4 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
             {isLoading && skeletonArrya.map((s) => <GameCardSkeleton key={s} />)}
-            {error && <p className="text-red-600 dark:text-pink-700 font-bold text-xl">{error}</p>}
             {data.length !== 0 &&
                 data.map((game) => <GameCard key={game.id} game={game} />)}
         </div>
